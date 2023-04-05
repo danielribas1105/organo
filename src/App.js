c97f0bda2d94ccf159dcc7d0d8f1c99fa7d85e25
Daniel <danielribas1105@gmail.com>
2023-04-05 19:10:33 -0300

    update project organo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <CampoTexto label="Nome" placeholder="Digite o nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite o cargo"/>
+      <CampoTexto label="Imagem" placeholder="Carrege a imagem"/>
     </div>
   );
 }
