b6b80978e32ef693691971202fe43268573ec186
Daniel <danielribas1105@gmail.com>
2023-04-06 10:38:20 -0300

    update form components

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -3,18 +3,23 @@ import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
+const aoClicar = (evento) => {
+    evento.preventDefault();
+    console.log("Form submetido!");
+}
+
 const Formulario = () => {
     const times = [
         'Programação','Front-end','Data Science','DevOps','UX e Design','Mobile','Inovação e Gestão'
     ];
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoClicar}>
                 <h2>Preencha os campos para gerar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o nome" />
-                <CampoTexto label="Cargo" placeholder="Digite o cargo" />
+                <CampoTexto required={true} label="Nome" placeholder="Digite o nome" />
+                <CampoTexto required={true} label="Cargo" placeholder="Digite o cargo" />
                 <CampoTexto label="Imagem" placeholder="Carrege a imagem" />
-                <ListaSuspensa label="Time" itens={times}/>
+                <ListaSuspensa required={true} label="Time" itens={times}/>
                 <Botao>
                     Criar card
                 </Botao>
