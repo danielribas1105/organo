f1b47c0c5d07cc00e73e226c03c94cb6a8c56296
Daniel <danielribas1105@gmail.com>
2023-05-15 11:09:40 -0300

    update react project

--- src/App.js
+++ src/App.js
@@ -6,43 +6,36 @@ import Rodape from './componentes/Rodape';
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState([
     {
       nome: 'Programação',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
+      cor: '#57C278',
     },
     {
       nome: 'Front-end',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      cor: '#82CFFA',
     },
     {
       nome: 'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      cor: '#A6D157',
     },
     {
       nome: 'DevOps',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      cor: '#E06B69',
     },
     {
       nome: 'UX e Design',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5'
+      cor: '#DB6EBF',
     },
     {
       nome: 'Mobile',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9'
+      cor: '#FFBA05',
     },
     {
       nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      cor: '#FF8A29',
     },
-  ];
+  ]);
 
   const [colaboradores, setColaboradores] = useState([]);
 
@@ -52,6 +45,15 @@ function App() {
 
   function deletarColaborador() {
     console.log("Deletar colaborador");
+  }
+
+  function mudarCorTime(cor, nomeTime){
+    setTimes(times.map(time => {
+      if(time.nome === nomeTime){
+        time.cor = cor;
+      }
+      return time;
+    }));
   }
 
   return (
@@ -64,10 +66,11 @@ function App() {
       {times.map(time => <Time 
           key={time.nome} 
           nome={time.nome} 
-          corPrimaria={time.corPrimaria} 
+          cor={time.cor} 
           corSecundaria={time.corSecundaria}
           colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} 
           aoDeletar={deletarColaborador}
+          mudarCor={mudarCorTime}
         />)
       }
       <Rodape/>
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,11 +1,17 @@
 import Colaborador from '../Colaborador';
 import './Time.css';
+import hexToRgba from 'hex-to-rgba';
 
 const Time = (props) => {
     return (
         (props.colaboradores.length > 0) ? 
-        <section className="time" style={{backgroundColor: props.corSecundaria}}>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+        <section className="time" 
+            style={{backgroundImage:"url(/imagens/fundo.png)", 
+                    backgroundColor: hexToRgba(props.cor, 0.4)}}>
+            <input type="color" className="input-cor" 
+                value={props.cor} 
+                onChange={evento => props.mudarCor(evento.target.value, props.nome)}/>
+            <h3 style={{borderColor: props.cor}}>{props.nome}</h3>
             <div className="colaboradores">
                 {props.colaboradores.map(colaborador => {
                     return <Colaborador 
@@ -13,7 +19,7 @@ const Time = (props) => {
                         nome={colaborador.nome}
                         cargo={colaborador.cargo}
                         imagem={colaborador.imagem}
-                        corDeFundo={props.corPrimaria}
+                        corDeFundo={props.cor}
                         aoDeletar={props.aoDeletar}
                     />
                 })}
